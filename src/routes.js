2d36678efcc3d2d354a448d9aeac5aba7533b12b
novalramdhani <novalfitrahramdhani@gmail.com>
2022-06-19 22:09:21 +0700

    wip

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,44 @@
-const routes = [];
+const { 
+    getAllBooksHandler,
+    getBooksByIdHandler,
+    addBookHandler,
+    editBooksByIdHandler,
+    deleteBooksByIdHandler
+ } = require('./handler');
+
+const routes = [
+    {
+        method: "GET",
+        path: "/",
+        handler: () => {
+            return `Hello welcome to bookshelf api backend`;
+        }
+    },
+    {
+        method: "GET",
+        path: "/books",
+        handler: getAllBooksHandler
+    },
+    {
+        method: 'GET',
+        path: '/books/{bookId}',
+        handler: getBooksByIdHandler,
+    },
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/books/{bookId}',
+        handler: editBooksByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{bookId}',
+        handler: deleteBooksByIdHandler,
+    },
+];
 
 module.exports = routes;
