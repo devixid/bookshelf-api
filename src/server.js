3ce6c02071ad3df2b8e22724825ec8b5865a2a54
novalramdhani <novalfitrahramdhani@gmail.com>
2022-06-19 21:53:11 +0700

    fix problem bug

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 const hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
     const server = hapi.server({
-        port: "",
-        host: "localhost"
+        port: 5000,
+        host: "localhost",
+        routes: {
+            cors: {
+                origin: ["*"]
+            },
+        },
     });
+
+    server.route(routes);
+    await server.start();
+    console.log(`Local development server berjalan pada ${server.info.uri}`);
 };
+
+init();
